d8ff8cc702abb27e403ff126e1f624bd2df09425
IliaOstanin <108222856+IliaOstanin@users.noreply.github.com>
2022-06-26 16:18:01 +0300

    v1.0.1

--- secrets.h
+++ secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "XXXXXX"		// имя сети Wi-Fi
+#define SECRET_SSID "XXXXXX"	// имя сети Wi-Fi
 #define SECRET_PASS "XXXXXX"	// пароль сети Wi-Fi
 
-#define SECRET_CH_ID XXXXXX			// номер канала ThingSpeak
-#define SECRET_WRITE_APIKEY "XXXXXX"   // ключ API для записи данных в ThingSpeak
+#define SECRET_CH_ID XXXXXX				// номер канала ThingSpeak
+#define SECRET_WRITE_APIKEY "XXXXXX"    // ключ API для записи данных в ThingSpeak
